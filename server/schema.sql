cf767352ecd55f8714a65b98b58284ce7617d257
tankwan <tankwantiong@gmail.com>
2016-10-07 15:16:19 -0700

    Generate sql code to build databases

--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,37 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `message` VARCHAR(500) NOT NULL DEFAULT '',
+  `createdAt` DATETIME NULL DEFAULT NULL,
+  `userid` INTEGER NULL DEFAULT NULL,
+  `roomid` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE `rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `roomname` VARCHAR(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+
+
+ALTER TABLE `messages` ADD FOREIGN KEY (userid) REFERENCES `users` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (roomid) REFERENCES `rooms` (`id`);
+
+
+
 
 
 
@@ -14,4 +39,3 @@ CREATE TABLE messages (
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
