2e378fd0d3466553a1490e5621c2105f878592d0
tankwan <tankwantiong@gmail.com>
2016-10-07 17:33:25 -0700

    Add code to db/index.js to fix mysql connections

--- server/db/index.js
+++ server/db/index.js
@@ -4,22 +4,28 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-var dbConnection = mysql.createConnection ({
-  user: 'root',
-  password: 'fintony',
-  database: 'chat'
-});
+var initializeDb = function() {
+  var dbConnection = mysql.createConnection ({
+    user: 'root',
+    password: 'fintony',
+    database: 'chat'
+  });
+  
+  dbConnection.connect();
+  return dbConnection;
+};
 
-dbConnection.connect();
 
 module.exports._getUserId = function(username, callback) {
   // insert message into database
   // username, messsage, room
   // generate createdAt
   // invoke callback when done
+  var dbConnection = initializeDb();
   dbConnection.query('SELECT id from users where username = :username', 
     { username: username },
     function(err, userId) {
+      dbConnection.end();
       if (err) { throw err; }
       callback(userId);
     });
@@ -27,8 +33,11 @@ module.exports._getUserId = function(username, callback) {
 };
 
 module.exports.getMessages = function(callback) {
+  
+  var dbConnection = initializeDb();
   dbConnection.query('SELECT u.username, m.message, m.roomname, m.createdAt FROM messages m INNER JOIN users u ON m.userId = u.id', 
     function(err, data) {
+      dbConnection.end();
       if (err) { 
         throw err; 
       } else {
@@ -42,11 +51,12 @@ module.exports.postMessage = function(data, callback) {
   // username, messsage, room
   // generate createdAt
   // invoke callback when done
-
-  this.getUserId(data.username, function(userId) {
+  var dbConnection = initializeDb();
+  dbConnection.getUserId(data.username, function(userId) {
     dbConnection.query('INSERT INTO messages SET message=:message, roomname=:roomname, userid=:userid', 
       { message: data.message, roomname: data.roomname, userid: userId }, 
       function(err) {
+        dbConnection.end();
         if (err) { 
           throw err; 
         } else {
@@ -60,9 +70,11 @@ module.exports.postUser = function(data, callback) {
   // insert user into database
   // username
   // invoke callback when done
+  var dbConnection = initializeDb();
   dbConnection.query('INSERT INTO users SET username=:username', 
     { username: data.username }, 
     function(err) {
+      dbConnection.end();
       if (err) { 
         throw err; 
       } else {
